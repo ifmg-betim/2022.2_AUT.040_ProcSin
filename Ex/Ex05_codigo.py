cb9b7d7e5c46e5beb23964fe9456e0466e000397
Leandro Freitas <leandrofabreu@users.noreply.github.com>
2022-09-28 10:42:08 -0300

    Update Ex05_codigo.py

--- Ex/Ex05_codigo.py
+++ Ex/Ex05_codigo.py
@@ -10,6 +10,6 @@ def capturaSinalSecreto(ar):
     t1, t2, t3 = np.arange(c1*48000,c1*48000+45000), np.arange(c2*48000,c2*48000+45000), np.arange(c3*48000,c3*48000+45000)
     T=np.hstack((v[t1],v[t2],v[t3])); m=np.arange(s(T))/fs; T=T+np.random.normal(0,1e3,s(T))
     c4, r1 = np.random.randint(5e3,20e3), np.random.normal(0,3e3,s(T))
-    d2,n1=signal.iirdesign([(c4-2000)/fs,(c4+2000)/fs],[(c4-800)/fs,(c4+800)/fs],1,40)
+    d2,n1=signal.iirdesign([(c4-2000)*2/fs,(c4+2000)*2/fs],[(c4-800)*2/fs,(c4+800)*2/fs],1,40)
     K=T*np.sin(2*np.pi*c4*m+10*np.random.rand())+signal.filtfilt(d2,n1,r1)
     return K,fs
